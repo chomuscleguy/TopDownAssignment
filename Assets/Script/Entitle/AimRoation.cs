742af17cf59f8b60f558b045acc8c4da08c2be6e
조성우 <chomuscleguy@gmail.com>
2023-11-29 18:09:04 +0900

    해설영상 후 수정

--- Assets/Script/Entitle/AimRoation.cs
+++ Assets/Script/Entitle/AimRoation.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class AimRotation : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer armRenderer;
-    [SerializeField] private Transform armPivot;
+    //[SerializeField] private SpriteRenderer armRenderer;
+    //[SerializeField] private Transform armPivot;
     [SerializeField] private SpriteRenderer characterRenderer;
 
     private CharacterController _controller;
@@ -29,8 +29,9 @@ public class AimRotation : MonoBehaviour
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
-        characterRenderer.flipX = armRenderer.flipY;
-        armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
+        characterRenderer.flipX = Mathf.Abs(rotZ) > 90f;
+        //armRenderer.flipY = Mathf.Abs(rotZ) > 90f;
+        //characterRenderer.flipX = armRenderer.flipY;
+        //armPivot.rotation = Quaternion.Euler(0, 0, rotZ);
     }
 }
